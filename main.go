9718765bbb9b484f7dacfba2282e4c40b35e7638
Terandium <contact@terandium.com>
2024-06-27 14:37:57 +0200

    feat: add font to executable

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"log"
 	"os"
-	"path"
-	"runtime"
 	"sync"
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/code128"
 	"github.com/fogleman/gg"
 	"github.com/xuri/excelize/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
+
+//go:embed fonts/DejaVuSans-Bold.ttf
+var myFont []byte
 
 func main() {
 	// Check if out folder exists, if not create it
@@ -67,15 +71,6 @@ func main() {
 	fmt.Println("Stickers generated!")
 }
 
-func getDirectory() string {
-	_, file, _, ok := runtime.Caller(1)
-	if ok {
-		return path.Dir(file)
-	}
-
-	return ""
-}
-
 func createBarcode(barcodeData string, customText string) {
 	// Generate the barcode
 	code, err := code128.Encode(barcodeData)
@@ -108,23 +103,28 @@ func createBarcode(barcodeData string, customText string) {
 	maxWidth := float64(width - 20) // 10 pixels padding on each side
 	maxFontSize := calculateMaxFontSize(dc, customText, maxWidth)
 
-	// Get the address of the current directory
-	address := getDirectory()
+	// Load the font face from the byte array
+	fontFace, err := loadFontFace(myFont, 14)
+	if err != nil {
+		panic(err)
+	}
 
 	// Draw the small barcode text underneath the barcode
 	dc.SetRGB(0, 0, 0)
-	if err := dc.LoadFontFace(address+"/DejaVuSans-Bold.ttf", 14); err != nil {
-		panic(err)
-	}
+	dc.SetFontFace(fontFace)
 
 	smallBarcode := trimBarcode(barcodeData)
 	dc.DrawStringAnchored(smallBarcode, width/2, height-32, 0.5, 0.5)
 
-	// Set text properties and draw the custom text below the barcode
-	dc.SetRGB(0, 0, 0)
-	if err := dc.LoadFontFace(address+"/DejaVuSans-Bold.ttf", maxFontSize); err != nil {
+	// Load the font face from the byte array
+	fontFace, err = loadFontFace(myFont, maxFontSize)
+	if err != nil {
 		panic(err)
 	}
+
+	// Set text properties and draw the custom text below the barcode
+	dc.SetRGB(0, 0, 0)
+	dc.SetFontFace(fontFace)
 	dc.DrawStringAnchored(customText, width/2, height-15, 0.5, 0.5)
 
 	// Save the image to a file
@@ -137,10 +137,12 @@ func createBarcode(barcodeData string, customText string) {
 func calculateMaxFontSize(dc *gg.Context, text string, maxWidth float64) float64 {
 	fontSize := 20.0
 	for {
-		address := getDirectory()
-		if err := dc.LoadFontFace(address+"/DejaVuSans-Bold.ttf", fontSize); err != nil {
+		// Load the font face from the byte array
+		fontFace, err := loadFontFace(myFont, fontSize)
+		if err != nil {
 			panic(err)
 		}
+		dc.SetFontFace(fontFace)
 		width, _ := dc.MeasureString(text)
 		if width <= maxWidth {
 			return fontSize
@@ -160,4 +162,19 @@ func trimBarcode(s string) string {
 
 	// Trim 8 characters from the front and 1 character from the end
 	return s[8 : len(s)-1]
+}
+
+func loadFontFace(data []byte, size float64) (font.Face, error) {
+	font, err := opentype.Parse(data)
+	if err != nil {
+		return nil, err
+	}
+	face, err := opentype.NewFace(font, &opentype.FaceOptions{
+		Size: size,
+		DPI:  72,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return face, nil
 }
